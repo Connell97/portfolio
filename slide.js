d2f5132d34c70fa04b2d307655ecdd40f00efa7a
Matt <m.connell404@gmail.com>
2021-07-05 18:21:42 +0100

    Created JS file for slide animation and removed unnecessary file

--- slide.js
+++ slide.js
@@ -1,14 +1,18 @@
-const info = document.querySelector('slide');
+const button = document.querySelector('#slide');
+const info = document.querySelector('#slide').parentElement;
+
 let clicked = false;
 
-info.addEventListener('click', slideFunc);
-
-slideFunc = () => {
+const slideFunc = () => {
     if (clicked == false) {
         info.classList.add("slide-animation");
+        clicked = true
+        console.log("clicked")
     }
     else {
         info.classList.remove("slide-animation");
         clicked = false;
     }
 }
+
+button.addEventListener('click', slideFunc);
